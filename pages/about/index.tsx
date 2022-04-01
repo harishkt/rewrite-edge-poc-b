d0caf8f23f90c6588fd0b452192287461ab9ec93
harishkt <harisht@udel.edu>
2022-04-01 13:56:08 -0400

    poc-b

---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export default function About() {
+
+    return(
+      <div>In About page of rewrite-edge-poc-b</div>  
+    );
+}
